aa763a4dcb43fb44a3c6de3b5a5b82a94566a1c2
callmeanto <antonellalrc@gmail.com>
2018-10-10 13:16:32 -0400

    implementacion de todo

--- sust_adel.m
+++ sust_adel.m
@@ -1,13 +1,13 @@
-% Sustitucion hacia atras
+% Sustitucion hacia adelate
 
-function x = sust_atras(A,b)
+function x = sust_adel(A,b)
   n=length(A);
   x=nan*ones(n,1);
-  x(n)=b(n)/A(n,n);
+  x(1)=b(1)/A(1,1);
   
-  for i=n-1:-1:1
+  for i=2:1:n
     suma=0;
-    for j=i+1:1:n
+    for j=1:1:i-1
       suma=suma+A(i,j)*x(j);
     end
     x(i)=(b(i)-suma)/A(i,i);
