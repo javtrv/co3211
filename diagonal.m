e7bdec94228c32a36b553a81a73a3397e91d0d0a
callmeanto <antonellalrc@gmail.com>
2018-10-10 11:57:53 -0400

    cambios en el algoritmo de diagonalizacion. Poseia errores de sintaxis

--- diagonal.m
+++ diagonal.m
@@ -1,9 +1,10 @@
-% Algoritmo para calcular matriz diagonal
+% Algoritmo para resolver sistema de ecuaciones Ax=b cuando A 
+% es una matriz diagonal
 
 function x=diagonal(A,b)
    n=length(A);
    x=nan*ones(n,1);
    for i=1:1:n
-      x(i)=b(i),A(i;i);
+      x(i)=b(i)/A(i,i);
     end
 endfunction
