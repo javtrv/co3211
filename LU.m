18ff7973260550a22742a8a0867f732fe43550ec
callmeanto <antonellalrc@gmail.com>
2018-10-17 13:06:29 -0400

    descomposicion LU

---
+++ LU.m
@@ -0,0 +1,15 @@
+function A = LU(A)
+
+  [m,n] = size(A);
+  for k=1:1:n
+    for i=k:1:n
+      alpha = A(i,k-1)/A(k-1,k-1);
+      A(i,k-1) = alpha;
+      
+      for j=k:1:n
+        A(i,j) = A(i,j) - (A(i,k-1)*A(k-1,j));
+      end
+    end
+  end
+  
+endfunction
