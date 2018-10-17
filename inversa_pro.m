af32ce0eece5ab90d2761792689616a7eb87857d
callmeanto <antonellalrc@gmail.com>
2018-10-17 14:29:14 -0400

    algoritmo de calculo de inversa mediante LU eficiente

---
+++ inversa_pro.m
@@ -0,0 +1,16 @@
+function inv = inversa_pro(A)
+
+    n = length(A);
+    Lu = LU(A);
+    L = tril(Lu,-1) + eye(n);
+    U = triu(Lu);
+    inv = [];
+    for k=1:1:n
+        e=0*ones(n,1);
+        e(k) = 1;
+        y = sust_adel(L,e);
+        x = sust_atras(U,y);
+        inv = [inv,x];
+    end
+    
+endfunction
